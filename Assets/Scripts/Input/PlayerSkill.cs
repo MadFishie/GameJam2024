689467abdb1f445f14ddca6fb3bd5d0d0a662a03
evdogwoofwoof <leblancevan@icloud.com>
2024-10-19 12:35:58 -0400

    FUNNY CODE
    HA HA HA GHOST...

--- Assets/Scripts/Input/PlayerSkill.cs
+++ Assets/Scripts/Input/PlayerSkill.cs
@@ -6,12 +6,17 @@ public class PlayerSkill:MonoBehaviour
 {
     
     PlayerMove player;
-    [SerializeField] Sprite PlayerSprite;
+    [SerializeField] SpriteRenderer PlayerSprite;
+    private static CircleCollider2D PlayerCollider;
+    [SerializeField] BoxCollider2D PlayerBoxCollider;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<PlayerMove>();
+        PlayerCollider = player.GetComponent<CircleCollider2D>();
+        PlayerBoxCollider = player.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -25,6 +30,9 @@ public class PlayerSkill:MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("AHHH");
+            PlayerSprite.color = Color.white;
+            PlayerCollider.radius = 0;
+            PlayerBoxCollider.isTrigger = false;
 
         }
     }
