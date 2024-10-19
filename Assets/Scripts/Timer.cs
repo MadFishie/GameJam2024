4046f2958bc9da681b8054062239495fd6934034
Doctornoodlearms <doctornoodlearms11@gmail.com>
2024-10-19 13:15:11 -0400

    time is time which tracks the current time of the in game time... ina  timley manner

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+
+	double realTime = 0.00;
+	int timeSec = 0;
+
+	[SerializeField] Text timerText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+		realTime += Time.deltaTime;
+		timeSec = Mathf.RoundToInt((float)(realTime % 60));
+
+		timerText.text = $"Time: {timeSec.ToString()}";
+	}
 }
