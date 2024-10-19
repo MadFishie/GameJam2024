3fcf9a8f39a7cd5590720ae10bc8ea23cdd9be30
evdogwoofwoof <leblancevan@icloud.com>
2024-10-19 14:28:55 -0400

    StealthMODE(Evan)
    FINALIZED

--- Assets/Scripts/Input/PlayerSkill.cs
+++ Assets/Scripts/Input/PlayerSkill.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerSkill:MonoBehaviour
@@ -9,31 +11,46 @@ public class PlayerSkill:MonoBehaviour
     [SerializeField] SpriteRenderer PlayerSprite;
     private static CircleCollider2D PlayerCollider;
     [SerializeField] BoxCollider2D PlayerBoxCollider;
-    
+    public bool Invis = false;
+    [SerializeField] TrailRenderer TrailRend;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<PlayerMove>();
         PlayerCollider = player.GetComponent<CircleCollider2D>();
-        PlayerBoxCollider = player.GetComponent<BoxCollider2D>();
+        PlayerBoxCollider = player.GetComponentInChildren<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        OnBecameInvisible();
+        if (Input.GetKeyDown("space")){
+            OnBecameInvisible();
+            Invis =!Invis;
+            
+        }
+        
     }
 
     public void OnBecameInvisible()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Invis == false)
         {
-            Debug.Log("AHHH");
-            PlayerSprite.color = Color.white;
+            PlayerSprite.color = new Color(188, 0, 255, 255);
+            PlayerCollider.radius = 0;
+            PlayerBoxCollider.isTrigger = true;
+            TrailRend.emitting = true;
+
+        }
+        else
+        {
+            PlayerSprite.color = new Color(188, 0, 255, 0.25f);
             PlayerCollider.radius = 0;
             PlayerBoxCollider.isTrigger = false;
-
+            TrailRend.emitting = false;
         }
     }
 }
