ea5c0b81b5d01eecb714e9ca5161882e9a2e48b0
MadFishie <125475305+MadFishie@users.noreply.github.com>
2024-10-19 13:49:27 -0400

    Gaslighting the code

--- Assets/Scripts/SwitchMode.cs
+++ Assets/Scripts/SwitchMode.cs
@@ -8,8 +8,10 @@ public class SwitchMode : MonoBehaviour
     private int AggressorCounter;
     [SerializeField] private int MinRange;
     [SerializeField] private int MaxRange;
-    [SerializeField] private int AttackMinRange;
-    [SerializeField] private int AttackMaxRange;
+    [SerializeField] private int AggressorTimeStart;
+    private int AggressorTimeRunning;
+    private int TimerSpice;
+    [SerializeField] public static int Timer;
 
 
     // Start is called before the first frame update
@@ -19,13 +21,27 @@ public class SwitchMode : MonoBehaviour
         StartCoroutine("AggressorCountDown");
     }
 
+    private void FixedUpdate()
+    {
+        if (IsAggressor == true)
+        {
+            AggressorTimeRunning -= TimerSpice;
+            if (AggressorTimeRunning % TimerSpice == 0) { Timer -= 1; }
+            if (AggressorTimeRunning == 0)
+            {
+                IsAggressor = false;
+                StartCoroutine("AggressorCountDown");
+            }
+        }
+    }
+
     IEnumerator AggressorCountDown() 
     {
         yield return new WaitForSeconds(AggressorCounter);
-        AggressorCounter = Random.Range(AttackMinRange, AttackMaxRange);
-        IsAggressor = true;
-        yield return new WaitForSeconds(AggressorCounter);
         AggressorCounter = Random.Range(MinRange, MaxRange);
         IsAggressor = true;
+        TimerSpice = Random.Range(30, 75);
+        AggressorTimeRunning = AggressorTimeStart * TimerSpice;
+        StopAllCoroutines();
     }
 }
